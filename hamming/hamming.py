a7fa96e8a5fdd9a4c44a54967e49a9290fe01e91
k17pine <47183551+k17pine@users.noreply.github.com>
2020-01-10 22:37:39 +0200

    hamming done

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,9 @@
 def distance(strand_a, strand_b):
-    pass
+    i = 0
+    if len(strand_a) == len(strand_b):
+        for j in range(len(strand_a)):
+            if strand_a[j] != strand_b[j]:
+                i += 1
+    else:
+        raise ValueError('ValueError')
+    return i
